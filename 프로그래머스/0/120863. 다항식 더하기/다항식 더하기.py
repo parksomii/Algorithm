e02f7d381a1fd8f9192a47b8f45f8ef2b51bc907
박소미 <somi020605@suwon.ac.kr>
2025-06-14 01:06:44 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,26 @@
+def solution(polynomial):
+    terms = polynomial.split(" + ")
+    x_sum = 0  # x항 계수의 합
+    num_sum = 0  # 숫자 항의 합
+
+    for term in terms:
+        if "x" in term:
+            if term == "x":
+                x_sum += 1
+            else:
+                x_sum += int(term.replace("x", ""))
+        else:
+            num_sum += int(term)
+
+    # 결과 문자열 만들기
+    result = ""
+
+    if x_sum > 0:
+        result += "x" if x_sum == 1 else str(x_sum) + "x"
+
+    if num_sum > 0:
+        if result:
+            result += " + "
+        result += str(num_sum)
+
+    return result
