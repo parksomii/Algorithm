4d2812e992b974391865561f6e1cfc42a12170b4
박소미 <somi020605@suwon.ac.kr>
2025-08-16 18:56:24 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,16 @@
+SELECT
+    B.TITLE, -- 게시글 제목
+    B.BOARD_ID, -- 게시글 ID
+    R.REPLY_ID, -- 댓글 ID
+    R.WRITER_ID, -- 댓글 작성자 ID
+    R.CONTENTS, -- 댓글 내용
+    DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE -- 댓글 작성일
+FROM
+    USED_GOODS_BOARD B
+JOIN
+    USED_GOODS_REPLY R ON B.BOARD_ID = R.BOARD_ID
+WHERE
+    DATE_FORMAT(B.CREATED_DATE, '%Y-%m') = '2022-10'
+ORDER BY
+    R.CREATED_DATE ASC,
+    B.TITLE ASC;
